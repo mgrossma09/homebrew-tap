ad9250438dff1f974f7ba7ca50d783cd3dfa51df
goreleaserbot <goreleaser@mgrossma09.com>
2026-02-19 03:36:51 +0000

    update c64-emulator to v0.0.1

--- Formula/c64-emulator.rb
+++ Formula/c64-emulator.rb
@@ -7,18 +7,18 @@ class C64Emulator < Formula
   on_macos do
     on_arm do
       url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_darwin_arm64.tar.gz"
-      sha256 "1032c0fb506d0420fc46b5a6c10bf9513b9759c5d8765ca410c719442571796d"
+      sha256 "7f29e07ae73aab4e98690047bf4ef7bd343e5a1d67938f5b34868d2ab4347e0b"
     end
     on_intel do
       url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_darwin_x86_64.tar.gz"
-      sha256 "b6340d3701a6d5e75bb2852e596862aa16ed4cd2d6261cd3a41382460eaa7f94"
+      sha256 "c6f673e88cad7f0df5eefeeabfc3ef47e3e3c8ad530380541f87a26e09819b4e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_linux_x86_64.tar.gz"
-      sha256 "3a77c8a1956e7fa3a15a8c9652ccb7028655d6e1411e4da623dcb92e2e095161"
+      sha256 "028bb33b0a8cb66db7486c0c3e668e313cb28222effaf78dff5af099b05f9019"
     end
   end
 
@@ -26,11 +26,13 @@ class C64Emulator < Formula
     bin.install "c64-emulator"
   end
 
-  caveats <<~EOS
-    c64-emulator requires Commodore 64 ROM files (basic.rom, kernal.rom, char.rom).
-    Run with:
-      c64-emulator -basic /path/to/basic.rom -kernal /path/to/kernal.rom -char /path/to/char.rom
-  EOS
+  def caveats
+    <<~EOS
+      c64-emulator requires Commodore 64 ROM files (basic.rom, kernal.rom, char.rom).
+      Run with:
+        c64-emulator -basic /path/to/basic.rom -kernal /path/to/kernal.rom -char /path/to/char.rom
+    EOS
+  end
 
   test do
     assert_predicate bin/"c64-emulator", :exist?
