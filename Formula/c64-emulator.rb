7d88b4c3247818ed440021a0a9cc89758553b15f
goreleaserbot <goreleaser@mgrossma09.com>
2026-02-19 03:41:58 +0000

    update c64-emulator to v0.0.2

--- Formula/c64-emulator.rb
+++ Formula/c64-emulator.rb
@@ -1,24 +1,24 @@
 class C64Emulator < Formula
   desc "Cycle-accurate Commodore 64 emulator written in Go"
   homepage "https://github.com/mgrossma09/c64-emulator-go"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_darwin_arm64.tar.gz"
-      sha256 "7f29e07ae73aab4e98690047bf4ef7bd343e5a1d67938f5b34868d2ab4347e0b"
+      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.2/c64-emulator_darwin_arm64.tar.gz"
+      sha256 "61681c771fa9af5d656375927ee58de8a0c011bad3b9beb6d6d52ee677b932e6"
     end
     on_intel do
-      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_darwin_x86_64.tar.gz"
-      sha256 "c6f673e88cad7f0df5eefeeabfc3ef47e3e3c8ad530380541f87a26e09819b4e"
+      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.2/c64-emulator_darwin_x86_64.tar.gz"
+      sha256 "acef985ff2167fb260f20bec560bdc9169e6b916f83518843e8e696bb32737e7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.1/c64-emulator_linux_x86_64.tar.gz"
-      sha256 "028bb33b0a8cb66db7486c0c3e668e313cb28222effaf78dff5af099b05f9019"
+      url "https://github.com/mgrossma09/c64-emulator-go/releases/download/v0.0.2/c64-emulator_linux_x86_64.tar.gz"
+      sha256 "5b98d13fac500e45075c91189a0b2fd70b4f2d999e7b24a2146d19caeeaf8bb6"
     end
   end
 
